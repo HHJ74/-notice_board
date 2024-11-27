972fa03a4c01df55cf7022ae9760c96e990b4b94
HHJ74 <gpgus0704@gmail.com>
2024-11-27 16:48:14 +0900

    refactor / Change post update method from PUT to PATCH

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -8,13 +8,13 @@ const postController = require('../controllers/postController');
 router.get('/users/:user_number/posts', postController.getUserPosts);
 
 // GET /posts/:post_id - 특정 게시물 조회
-router.get('/posts/:post_id', postController.getPostByPostNum);
+router.get('/posts/:post_number', postController.getPostByPostNum);
 
 // POST /users/:user_number/posts - 특정 사용자의 게시글 생성
 router.post('/users/:user_number/posts', postController.createUserPosts);
 
 // POST /posts/:post_number - 게시글 수정
-router.put('/posts/:post_number', postController.updatePost);
+router.patch('/posts/:post_number', postController.updatePost);
 
 // 게시글 삭제
 router.delete('/posts/:post_number', postController.deletePost);
